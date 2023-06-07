b7c699cb2da8e2f502aa4a8cb9b698888c0c1c03
cicidoll <ayaseemt@qq.com>
2023-06-07 15:06:03 +0800

    update_stringMethods_convert

--- StringMethods/Convert.py
+++ StringMethods/Convert.py
@@ -1,4 +1,4 @@
-import base64
+import base64, re
 
 class Convert:
     """ 字符串格式转换方法 """
@@ -17,3 +17,9 @@ class Convert:
     def base64_convert_hex(input_base64: str) -> str:
         """ 将base64转换为hex字符串 """
         return str(base64.b64decode(input_base64).hex())
+    
+    @staticmethod
+    def is_base64(input_str):
+        """ 检测字符串是否为Base64编码 """
+        base64_code = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$"
+        return bool(re.match(base64_code, input_str))
